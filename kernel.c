d7e6a0518e241459a3f93b7538e32a6641a88eff
Timmy Sjöstedt <me@iostream.se>
2013-09-24 04:53:26 +0200

    Added C strlen and printString routines.

--- kernel.c
+++ kernel.c
@@ -1,5 +1,20 @@
-void main() {
+#include <stddef.h>
+
+size_t strlen(char *str) {
+	size_t len = 0;
+	while (*str++)
+		len++;
+	return len;
+}
+
+void printString(char *str) {
 	char *videomem = (char *) 0xb8000;
-	*videomem = 'X';
+	size_t len = strlen(str);
+	for (int i = 0; i < len; i++)
+		videomem[i*2] = str[i];
+}
+
+void main() {
+	printString("Hello World from the booted C kernel!");
 }
 
