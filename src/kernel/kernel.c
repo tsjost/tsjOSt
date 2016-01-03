a2bb794294e852f7c4f7c18a00c96f1a3afe1ea6
Timmy Sjöstedt <git@timsjo.se>
2016-01-03 11:25:49 +0100

    Made printString print on the next available line!

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,13 +3,16 @@
 #include <string.h>
 
 void printString(char *str) {
-	char *videomem = (char *) 0xb8000;
+	static unsigned char line = 0;
+	char *videomem = (char *) 0xb8000 + line*160;
 	size_t len = strlen(str);
 	for (size_t i = 0; i < len; i++)
 		videomem[i*2] = str[i];
+	++line;
 }
 
 void main() {
 	printString("Hello World from the booted C kernel!");
+	printString("And here's another line!");
 }
 
