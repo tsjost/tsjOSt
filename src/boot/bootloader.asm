9ecc00ecf58e224c66eec1717b09038df0a19a9c
Timmy Sjöstedt <git@timsjo.se>
2016-01-03 14:29:47 +0100

    Moved Global Descriptor Table down, because it is data

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -111,34 +111,6 @@ print_hex:
 
 	ret
 
-; Global Descriptor Table
-gdt_start:
-gdt_null:
-	dd 0x0
-	dd 0x0
-gdt_code:
-	dw 0xffff
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
-gdt_data:
-	dw 0xffff
-	dw 0x0
-	db 0x0
-	db 10010010b
-	db 11001111b
-	db 0x0
-gdt_end:
-
-gdt_descriptor:
-	dw gdt_end - gdt_start - 1
-	dd gdt_start
-
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
 ; 32 bit protected mode!
 BITS 32
 init_protected_mode:
@@ -181,6 +153,34 @@ print_string_protected:
 	ret
 
 ; Data
+
+; Global Descriptor Table
+gdt_start:
+gdt_null:
+	dd 0x0
+	dd 0x0
+gdt_code:
+	dw 0xffff
+	dw 0x0
+	db 0x0
+	db 10011010b
+	db 11001111b
+	db 0x0
+gdt_data:
+	dw 0xffff
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+gdt_end:
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 BOOT_DRIVE: db 0
 
 STR_HELLO:
