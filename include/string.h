28544e12a98f85e91b53342eee6ac10aadfaadfa
Timmy Sjöstedt <git@timsjo.se>
2016-01-04 13:15:57 +0100

    Implemented memcpy

--- include/string.h
+++ include/string.h
@@ -4,3 +4,4 @@
 
 size_t strlen(char *str);
 
+void *memcpy(void * restrict dest, const void * restrict src, size_t n);
--- src/kernel/string.c
+++ src/kernel/string.c
@@ -7,3 +7,14 @@ size_t strlen(char *str) {
     return len;
 }
 
+void *memcpy(void * restrict dest, const void * restrict src, size_t n)
+{
+	char * restrict dest_c = dest;
+	const char * restrict src_c = src;
+
+	for (size_t i = 0; i < n; ++i) {
+		dest_c[i] = src_c[i];
+	}
+
+	return dest;
+}
